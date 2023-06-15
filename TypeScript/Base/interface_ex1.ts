104c8f92aef6d10ff0d10cf4371cccf933964a16
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2023-06-15 16:38:09 +0900

    Update interface_ex1.ts

--- TypeScript/Base/interface_ex1.ts
+++ TypeScript/Base/interface_ex1.ts
@@ -1,1 +1,16 @@
+interface Song {
+  songName: String;
+  songRelease: number;
+  albumName?: String;  // 可選變數
+}
+//  可選變數範例 
+let influencer: Song = {
+  songName: "Influencer",
+  songRelease: 2022,
+  albumName: "Unleash"
+}
 
+let aboutUs: Song = {
+  songName: "About Us",
+  songRelease: 2021
+}
